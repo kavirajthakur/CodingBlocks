8967a1c683b6a759e53c0339ddc5237bb91886bb
kaviraj thakur <kavirajthakurofficial@gmail.com>
2023-08-11 19:38:31 +0530

    chnages

---
+++ src/StackQue/Next_Greater_Element.java
@@ -0,0 +1,41 @@
+package StackQue;
+import java.util.*;
+
+import java.util.Stack;
+
+public class Next_Greater_Element {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 11, 3, 23, 9, 15 };
+		NGE(arr);
+
+	}
+
+	public static void NGE(int[] arr) {
+
+		int[] nge = new int[arr.length];
+		Stack<Integer> st = new Stack<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			// nge
+			while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
+				int ii = st.pop();
+				nge[ii] = arr[i];
+			}
+
+			st.push(i);
+
+		}
+
+		while (!st.isEmpty()) {
+			int ii = st.pop();
+			nge[ii] = -1;
+		}
+
+		for (int i = 0; i < nge.length; i++) {
+			System.out.println(arr[i] + " " + nge[i]);
+
+		}
+	}
+
+}
